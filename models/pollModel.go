9d3da5228800da2115c9a4c4f509eedc8e8d91b6
Bayu Nashrullah <nashr.bayu77@gmail.com>
2023-01-15 14:00:27 +0700

    db revision

--- models/pollModel.go
+++ models/pollModel.go
@@ -7,24 +7,24 @@ type Poll struct {
 	Subject     string `gorm:"unique;not null"`
 	Description string
 	TotalVote   int
-	OwnedBy     int
-	UserOwner   User `gorm:"foreignKey:OwnedBy"`
+	UserID      int
+	User        User
 }
 
 type PollChoice struct {
 	gorm.Model
 	Choice    string `gorm:"not null"`
 	TotalVote int
-	OwnedBy   int
-	PollOwner Poll `gorm:"foreignKey:OwnedBy"`
+	PollID    int
+	Poll      Poll
 }
 
 type PollEntry struct {
 	gorm.Model
-	VoteBy      string
-	VoteOn      int
-	VoteOwnedBy int
-	UserVoted   User       `gorm:"foreignKey:VoteBy"`
-	ChoiceVoted PollChoice `gorm:"foreignKey:VoteOn"`
-	PollOwner   Poll       `gorm:"foreignKey:VoteOwnedBy"`
+	UserID       int
+	PollChoiceID int
+	PollID       int
+	User         User
+	PollChoice   PollChoice
+	Poll         Poll
 }
