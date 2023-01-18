9ff1c29bdae8f5ade2cf9ad87fa00aca05355ca0
Bayu Nashrullah <nashr.bayu77@gmail.com>
2023-01-18 18:58:29 +0700

    add hot polling to home

--- controllers/publicController.go
+++ controllers/publicController.go
@@ -1,9 +1,21 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/bayunashr/gopoll/initializers"
+	"github.com/bayunashr/gopoll/models"
+	"github.com/gin-gonic/gin"
+)
 
 func PublicHome(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "success, this is home",
-	})
+	var hotPoll []models.Poll
+	result := initializers.DB.Where("visibility = true AND archive = false").Order("total_vote desc").Limit(10).Find(&hotPoll)
+	if result.Error != nil {
+		c.JSON(400, gin.H{
+			"message": "error, fail to read poll",
+		})
+	} else {
+		c.JSON(200, gin.H{
+			"data": hotPoll,
+		})
+	}
 }
